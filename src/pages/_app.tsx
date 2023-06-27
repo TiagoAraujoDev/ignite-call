72ebc855e742c7101b2989af38902c3bb2692c9f
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2023-06-27 23:07:56 +0100

    chore: Setup @ignite-ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Roboto_Flex as Roboto } from "next/font/google";
+
+import { globalStyles } from '@/styles/globals'
+
+const roboto = Roboto({ subsets: ['latin'], weight: ['400', "500", '700'], display: 'swap' });
+
+globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
